9355187398ebcb6bcc033491f509f35916c0907b
jessicabonner <jessicabonner@utexas.edu>
2023-03-16 13:02:18 -0700

    centrality testing

--- scripts/network/testing_osmnx.py
+++ scripts/network/testing_osmnx.py
@@ -1,4 +1,5 @@
 import osmnx as ox
+import networkx as nx
 import os
 import sys
 
@@ -30,11 +31,60 @@ def walking_street_length():
     print(stats_walk)
 
 
+def get_graph_from_bb(bbox, walk_drive):
+    G = ox.graph.graph_from_bbox(bbox[0], bbox[1],bbox[2], bbox[3], network_type = walk_drive, simplify=True, retain_all=False)
+    return G
+
+
+def get_centrality(bbox, walk_drive):
+    
+    G = get_graph_from_bb(bbox, walk_drive)
+    basic_stats = ox.basic_stats(G)
+    print("circuity avg: ")
+    print(basic_stats['circuity_avg'])
+
+    G2 = nx.DiGraph(G)
+
+    #extended_stats = ox.extended_stats(G, bc=True)
+    print("betweenness centrality: ")
+    bet_centrality = nx.betweenness_centrality(G2, normalized = True, endpoints=False)
+    #print(bet_centrality)
+    print(avg(bet_centrality))
+
+    print("page rank")
+    pr_centrality = nx.pagerank(G2)
+    #print(pr_centrality)
+    print(avg(pr_centrality))
+
+def avg(evaluation_dictionary):
+    vals = evaluation_dictionary.values()
+    return sum(vals)/len(vals)
+
+def get_centrality_comparisons(bbox):
+    print("walking stats")
+    print("--------")
+    get_centrality(bbox, "walk")
+    print("driving stats")
+    print("--------")
+    get_centrality(bbox, "drive")
+
+
+
+
+#Centrality tests
+#get_centrality_comparisons([47.6526, 47.6493,-122.3489, -122.3548])
+
+#get_centrality_comparisons([47.65560, 47.65390,-122.34432, -122.34624])
+
+#get_centrality_comparisons([47.75988, 47.75765, -122.34003, -122.34333])
+
+
+# Network length tests
 #testing_compare()
 #walking_street_length()
-testing()
+#testing()
 
-
+#Output
 '''
 https://www.openstreetmap.org/export#map=16/47.6513/-122.3515&layers=T
 '''
