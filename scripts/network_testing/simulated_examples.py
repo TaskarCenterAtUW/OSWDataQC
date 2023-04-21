03fe66c22dc2781515b284dcb003e8ab3adc1e32
jessicabonner <jessicabonner@utexas.edu>
2023-04-21 12:26:37 -0700

    adding centrality metrics, csv save, graph save

--- scripts/network_testing/simulated_examples.py
+++ scripts/network_testing/simulated_examples.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import csv
 
 cases = {
     1:
@@ -16,38 +17,59 @@ def create_graph(case_number):
     for edge in cases[case_number]["edges"]:
         G.add_edge(edge[0], edge[1])
     
-    draw_graph(G)
+    draw_graph(G, f"original_case_{case_number}.png")
     return G
 
 
 def get_calculations():
+    centrality_stats = {}
     for case_number, case in cases.items():
         G = create_graph(case_number)
-        edge_calculations(case, G)
-        node_calculations(case, G)
-        #save list to csv
+        edge_stats = edge_calculations(case, G, case_number)
+        node_stats = node_calculations(case, G, case_number)
+        centrality_stats[case_number] = {
+            "edge_stats": edge_stats,
+            "node_stats": node_stats
+        }
+        #save lists to csv
+        save_csv(edge_stats, f"edge_{case_number}.csv")
+        save_csv(node_stats, f"node_{case_number}.csv")
 
-def edge_calculations(case, G):
+def save_csv(list_of_dict,filename):
+    with open(filename, 'w') as csvfile: 
+        csvwriter = csv.writer(csvfile) 
+        csvwriter.writerow(list_of_dict[0].keys())
+        for row in list_of_dict:
+            csvwriter.writerow(row.values())
+
+def edge_calculations(case, G, case_number):
+    original_centrality_stats = get_centrality(G)
+    original_centrality_stats["broken_edge"]="none"
+    stats = [original_centrality_stats]
     for edge in case["edges"]:
         print(edge)
         node_1,node_2 = break_edge(edge, G)
-        #draw_graph(G)
-        # get_centrality_metrics()
+        draw_graph(G, f"case_{case_number}_broken_edge_{edge}.png")
+        edge_stats = get_centrality(G)
+        edge_stats["broken_edge"] = edge
+        stats.append(edge_stats)
         # add centrality metrics to list
         connect_edge(G, edge, node_1, node_2)
         #draw_graph(G)
-    # output list
+    return stats
 
-def draw_graph(G):
+def draw_graph(G, filename):
+    fig = plt.figure()
     pos=nx.get_node_attributes(G,'pos')
-    nx.draw(G,pos)
-    plt.show()
-    #TODO: add drawing save
+    nx.draw(G,pos,ax=fig.add_subplot())
+    fig.savefig(filename)
+    #plt.show()
 
-def draw_graph_without_geo(G):
-    nx.draw(G)
-    plt.show()
-    #TODO: add drawing save
+def draw_graph_without_geo(G, filename):
+    fig = plt.figure()
+    nx.draw(G,ax=fig.add_subplot())
+    fig.savefig(filename)
+    #plt.show()
 
 def break_edge(edge_tuple, G):
     node_1 = edge_tuple[0]
@@ -81,15 +103,20 @@ def connect_edge(G, edge_tuple, node_1, node_2):
     G.add_edge(edge_tuple[0],edge_tuple[1])
 
 
-def node_calculations(case, G):
+def node_calculations(case, G, case_number):
+    original_centrality_stats = get_centrality(G)
+    original_centrality_stats["broken_node"]="none"
+    stats = [original_centrality_stats]
     for node_number, node in case["nodes"].items():
         edge_list, node_list = break_node(case["edges"], node, node_number, G)
-        # get_centrality_metrics()
-        # add centrality metrics to list
-        # save image
+        node_stats = get_centrality(G)
+        node_stats["broken_node"] = node_number
+        stats.append(node_stats)
+        draw_graph(G, f"case_{case_number}_broken_node_{node_number}.png")
+        draw_graph_without_geo(G, f"case_{case_number}_broken_node_{node_number}_no_geo.png")
         connect_node(edge_list, node_list, G, node_number, node)
-        draw_graph(G)
-    # output list
+        #draw_graph(G)
+    return stats
 
 def break_node(edges, node, node_number, G):
     edges_with_node = []
@@ -104,7 +131,7 @@ def break_node(edges, node, node_number, G):
             edges_with_node.append(edge)
             new_node_list.append(new_node)
     G.remove_node(node_number)
-    draw_graph(G)
+    #draw_graph(G)
     return edges_with_node, new_node_list
     # return new node list
 
@@ -117,4 +144,24 @@ def connect_node(edge_list, node_list, G, node_orig, node_orig_coordinates):
     
     # reconect all edges for node
 
+def get_centrality(G):
+    stats = {}
+    undirected_g = nx.Graph(G)
+
+    bet = nx.betweenness_centrality(undirected_g, normalized = True, endpoints=False)
+    eigen = nx.eigenvector_centrality(undirected_g, max_iter=2000)
+    deg = nx.degree_centrality(undirected_g)
+    stats["bet_centrality_avg"] = avg(bet)
+    stats["eig_centrality_avg"] = avg(eigen)
+    stats["deg_centrality_avg"] = avg(deg)
+    stats["connected"] = nx.is_connected(undirected_g)
+
+    return stats
+
+def avg(evaluation_dictionary):
+    vals = evaluation_dictionary.values()
+    return sum(vals)/len(vals)
+
 get_calculations()
+
+
