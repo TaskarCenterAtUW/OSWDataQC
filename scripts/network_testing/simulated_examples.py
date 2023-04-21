fa87b447c15c0aebd862f46e2a5bde00e1290344
jessicabonner <jessicabonner@utexas.edu>
2023-04-21 15:53:20 -0700

    add all cases and don't break nodes if they aren't connecting nodes

--- scripts/network_testing/simulated_examples.py
+++ scripts/network_testing/simulated_examples.py
@@ -1,12 +1,33 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import csv
+import os
 
 cases = {
     1:
     {
         "nodes": { 1: (1,1), 2: (2,1), 3: (3,1), 4: (3,2), 5: (2,2), 6: (1,2)},
         "edges": [(1,2), (2,3), (3,4), (4,5), (5,6), (6,1), (2,5)]
+    },
+    2: {
+        "nodes": { 1: (1,1), 2: (2,1), 3: (3,1), 4: (3,2), 5: (2,2), 6: (1,2)},
+        "edges": [(1,2), (2,3), (3,4), (4,5), (5,6), (6,1)]
+    },
+    3: {
+        "nodes": { 1: (1,1), 2: (2,1), 3: (3,1), 4: (3,2), 5: (2,2), 6: (1,2), 7: (3,4), 8: (4,4)},
+        "edges": [(1,2), (2,3), (3,4), (4,5), (5,6), (6,1), (7,8)]
+    },
+    4: {
+        "nodes": { 1: (1,1), 2: (2,1), 3: (3,1), 4: (3,2), 5: (2,2), 6: (1,2), 7: (3,3), 8: (3,4), 9:(2,4)},
+        "edges": [(1,2), (2,3), (3,4), (4,5), (5,6), (6,1), (4,7), (7,8),(8,9),(9,4)]
+    },
+    5: {
+        "nodes": {1: (2,2), 2: (1,2), 3: (1.25,2.75), 4: (2,3), 5: (2.75,2.75), 6: (3,2), 7: (2.75,1.25), 8: (2,1), 9: (1.25,1.25) },
+        "edges": [(2,3),(3,4),(4,5),(5,6),(6,7),(7,8),(8,9),(9,2), (1,2), (1,3),(1,4),(1,5),(1,6),(1,7),(1,8),(1,9)]
+    },
+    6: {
+        "nodes": {1: (1,1), 2: (1,2), 3: (1,3), 4:(2,3), 5: (2,2), 6: (2,1), 7: (3,1), 8: (3,2), 9: (3,3), 10: (4,1)},
+        "edges": [(1,2), (2,3), (3,4), (4,9), (9,8), (8,7), (7,6), (6,1), (1,10), (4,5), (5,8) ]
     }
 }
 
@@ -17,13 +38,15 @@ def create_graph(case_number):
     for edge in cases[case_number]["edges"]:
         G.add_edge(edge[0], edge[1])
     
-    draw_graph(G, f"original_case_{case_number}.png")
+    draw_graph(G, f"case_{case_number}/original.png")
     return G
 
 
 def get_calculations():
     centrality_stats = {}
     for case_number, case in cases.items():
+        if not os.path.isdir(f"./case_{case_number}"):
+            os.mkdir(f"./case_{case_number}")
         G = create_graph(case_number)
         edge_stats = edge_calculations(case, G, case_number)
         node_stats = node_calculations(case, G, case_number)
@@ -32,8 +55,8 @@ def get_calculations():
             "node_stats": node_stats
         }
         #save lists to csv
-        save_csv(edge_stats, f"edge_{case_number}.csv")
-        save_csv(node_stats, f"node_{case_number}.csv")
+        save_csv(edge_stats, f"case_{case_number}/edge_{case_number}.csv")
+        save_csv(node_stats, f"case_{case_number}/node_{case_number}.csv")
 
 def save_csv(list_of_dict,filename):
     with open(filename, 'w') as csvfile: 
@@ -47,9 +70,9 @@ def edge_calculations(case, G, case_number):
     original_centrality_stats["broken_edge"]="none"
     stats = [original_centrality_stats]
     for edge in case["edges"]:
-        print(edge)
+        #print(edge)
         node_1,node_2 = break_edge(edge, G)
-        draw_graph(G, f"case_{case_number}_broken_edge_{edge}.png")
+        draw_graph(G, f"case_{case_number}/broken_edge_{edge}.png")
         edge_stats = get_centrality(G)
         edge_stats["broken_edge"] = edge
         stats.append(edge_stats)
@@ -61,13 +84,13 @@ def edge_calculations(case, G, case_number):
 def draw_graph(G, filename):
     fig = plt.figure()
     pos=nx.get_node_attributes(G,'pos')
-    nx.draw(G,pos,ax=fig.add_subplot())
+    nx.draw(G,pos,ax=fig.add_subplot(), with_labels = True)
     fig.savefig(filename)
     #plt.show()
 
 def draw_graph_without_geo(G, filename):
     fig = plt.figure()
-    nx.draw(G,ax=fig.add_subplot())
+    nx.draw(G,ax=fig.add_subplot(), with_labels = True)
     fig.savefig(filename)
     #plt.show()
 
@@ -77,7 +100,7 @@ def break_edge(edge_tuple, G):
     midpoint = (G.nodes[node_1]['pos'][0] + G.nodes[node_2]['pos'][0]) / 2, \
            (G.nodes[node_1]['pos'][1] + G.nodes[node_2]['pos'][1]) / 2
     
-    print(midpoint)
+    #print(midpoint)
     # Calculate the coordinates of the two new nodes
     dist = .1  # Distance from the midpoint
     new_pos1 = ((midpoint[0] + dist * (G.nodes[node_2]['pos'][0] - G.nodes[node_1]['pos'][0]) / 2),
@@ -109,17 +132,17 @@ def node_calculations(case, G, case_number):
     stats = [original_centrality_stats]
     for node_number, node in case["nodes"].items():
         edge_list, node_list = break_node(case["edges"], node, node_number, G)
-        node_stats = get_centrality(G)
-        node_stats["broken_node"] = node_number
-        stats.append(node_stats)
-        draw_graph(G, f"case_{case_number}_broken_node_{node_number}.png")
-        draw_graph_without_geo(G, f"case_{case_number}_broken_node_{node_number}_no_geo.png")
+        if len(edge_list) > 1:
+            node_stats = get_centrality(G)
+            node_stats["broken_node"] = node_number
+            stats.append(node_stats)
+            draw_graph(G, f"case_{case_number}/broken_node_{node_number}.png")
+            draw_graph_without_geo(G, f"case_{case_number}/broken_node_{node_number}_no_geo.png")
         connect_node(edge_list, node_list, G, node_number, node)
-        #draw_graph(G)
     return stats
 
 def break_node(edges, node, node_number, G):
-    edges_with_node = []
+    edges_with_node = [] 
     new_node_list = []
     for edge in edges:
         if node_number in edge:
@@ -131,9 +154,9 @@ def break_node(edges, node, node_number, G):
             edges_with_node.append(edge)
             new_node_list.append(new_node)
     G.remove_node(node_number)
-    #draw_graph(G)
+
     return edges_with_node, new_node_list
-    # return new node list
+
 
 def connect_node(edge_list, node_list, G, node_orig, node_orig_coordinates):
     for node_to_delete in node_list:
