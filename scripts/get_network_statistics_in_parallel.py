c9acc288def1fce4d7aa819c5892f3ee372dd366
jessicabonner <jessicabonner@utexas.edu>
2023-08-15 11:59:18 -0700

    update centrality code

--- scripts/get_network_statistics_in_parallel.py
+++ scripts/get_network_statistics_in_parallel.py
@@ -5,6 +5,7 @@ import geopandas as gpd
 import os
 import osmnx as ox
 import dask_geopandas
+from statistics import stdev, mean
 
 
 def func( feature ):
@@ -14,6 +15,7 @@ def func( feature ):
         feature.degree = measures["deg_centrality_avg"]
         feature.eigen = measures["eig_centrality_avg"]
         feature.betweenness = measures["bet_centrality_avg"]
+        feature.bet_stdev = measures["bet_stdev"]
         return feature
 
 def analyze_area(filename, path = 'pwd'):
@@ -24,29 +26,33 @@ def analyze_area(filename, path = 'pwd'):
     gdf['degree'] = None
     gdf['betweenness'] = None
     gdf['eigen'] = None
+    gdf['bet_stdev'] = None
     
     df_dask = dask_geopandas.from_geopandas(gdf, npartitions=16)  
     if __name__ == '__main__':
         output = df_dask.apply(func, axis=1, meta=[
-            ('OBJECTID','int64'), 
-            ('Shape_Length', 'float64'), 
+            ('OBJECTID', 'int64'),
+            ('Shape_Length', 'float64'),
             ('Shape_Area','float64'), 
             ('Input_FID','int64'), 
-            ('geometry', 'geometry'), 
-            ('degree','object'), 
+            ('geometry', 'geometry'),
+            ('degree','object'),
             ('betweenness', 'object'), 
-            ('eigen', 'object')
+            ('eigen', 'object'),
+            ('bet_stdev', 'object'),
             ]).compute(scheduler='multiprocessing')
         
-        output.to_file(os.path.join(path, (filename + '_all_measures.shp')))
+        print("outputting file")
+        
+        output.to_file(os.path.join("C:\\Development\\tdei\\outputs", (filename + '_all_measures.shp')))
 
     
 def get_measures_from_polygon(polygon):
     try:
-        G = ox.graph.graph_from_polygon(polygon, custom_filter = '["highway"~"footway"]', truncate_by_edge=True, simplify=False, retain_all=True)
+        G = ox.graph.graph_from_polygon(polygon, custom_filter = '["highway"~"footway|steps"]', truncate_by_edge=True, simplify=False, retain_all=True)
     except Exception as e:
         print(f"Unexpected {e}, {type(e)} with polygon {polygon} when getting graph")
-        return {"bet_centrality_avg": None,"eig_centrality_avg": None,"deg_centrality_avg": None}
+        return {"bet_centrality_avg": None,"eig_centrality_avg": None,"deg_centrality_avg": None, "bet_stdev": None}
     stats = get_centrality(G, polygon)
     return stats
 
@@ -56,20 +62,21 @@ def get_centrality(G, polygon):
 
     bet = nx.betweenness_centrality(undirected_g, normalized = True, endpoints=False)
     try:
-        eigen = nx.eigenvector_centrality(undirected_g, max_iter=500)
-        stats["eig_centrality_avg"] = avg(eigen)
+        eigen = nx.eigenvector_centrality(undirected_g, max_iter=1000)
+        stats["eig_centrality_avg"] = mean(eigen.values())
     except Exception as e:
         print(f"Unexpected {e}, {type(e)} with polygon {polygon} when getting eigen value")
         stats["eig_centrality_avg"] = None
 
     deg = nx.degree_centrality(undirected_g)
-    stats["bet_centrality_avg"] = avg(bet)
-    stats["deg_centrality_avg"] = avg(deg)
+    stats["bet_centrality_avg"] = mean(bet.values())
+    stats["deg_centrality_avg"] = mean(deg.values())
+    stats["bet_stdev"] = stdev(bet.values())
     return stats
 
-def avg(evaluation_dictionary):
-    vals = evaluation_dictionary.values()
-    return sum(vals)/len(vals)
-
-
-analyze_area("seattle_new_tiling", "C:\\Users\\jessica\\Documents\\ArcGIS\\Projects\\MappingSidewalkMetrics\\")
+analyze_area("redmond_new_tiling","C:\\Users\\jessica\\Documents\\work\\redmond")
+#analyze_area("seattle_new_tiling","C:\\Users\\jessica\\Documents\\ArcGIS\\Projects\\MappingSidewalkMetrics")
+#analyze_area("seattle_crossing_tasks", "C:\\Development\\tdei\\OSWDataQC")
+#analyze_area("bellevue_crossing_tasks", "C:\\Development\\tdei\\OSWDataQC")
+#analyze_area("seattle_sidewalk_tasks", "C:\\Development\\tdei\\OSWDataQC")
+#analyze_area("bellevue_sidewalk_tasks", "C:\\Development\\tdei\\OSWDataQC")
